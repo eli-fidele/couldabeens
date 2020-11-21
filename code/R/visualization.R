7007fe20d3a229ad1799d351ecde884deaa41f36
Ali <alif.taqi@hotmail.com>
2020-11-20 22:42:12 -0800

    push

--- code/R/visualization.R
+++ code/R/visualization.R
@@ -1,1 +1,13 @@
 # Insert single-use visualization scripts/functions here
+
+# base visualization functions
+hist.var <- function(var, dataframe, title = ""){
+  ggplot(data = dataframe) + 
+    geom_histogram(aes_string(x = var)) + 
+    labs(title = title)
+}
+dens.var <- function(var, dataframe, title = ""){
+  ggplot(data = dataframe) + 
+    geom_density(aes_string(x = var)) +
+    labs(title = title)
+}
--- code/R/wrangle.R
+++ code/R/wrangle.R
@@ -1,13 +1,1 @@
 # Insert single-use wrangling scripts/functions here
-
-# base visualization functions
-hist.var <- function(var, dataframe, title = ""){
-  ggplot(data = dataframe) + 
-    geom_histogram(aes_string(x = var)) + 
-    labs(title = title)
-}
-dens.var <- function(var, dataframe, title = ""){
-  ggplot(data = dataframe) + 
-    geom_density(aes_string(x = var)) +
-    labs(title = title)
-}
